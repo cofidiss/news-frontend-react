308cecdf0bc4cd7ae0f686896bcb24c02d5772b9
sb <sb@450G7>
2023-03-21 21:54:17 +0300

    dummy data table eklendi

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Login from "../src/components/Login/Login";
 import CommentList from "../src/components/Comment/CommentList/CommentList";
 import NewsCard from "../src/components/NewsCard/NewsCard";
 import AddNews from "../src/components/AddNews/AddNews";
+import NewsList from "../src/components/NewsList/NewsList";
 function App() {
   const baseUrl="http://localhost:40774/asdas/asd";
   return (
     <div >
-     <NewsCard newsId={11}  baseUrl={baseUrl}/>
-     <AddNews categoryId={1}  baseUrl={baseUrl}/>
+     {/* <NewsCard newsId={11}  baseUrl={baseUrl}/>
+     <AddNews categoryId={1}  baseUrl={baseUrl}/> */}
+     <NewsList NewsList={[{header:"heeader1",id:1},{header:"heeader2",id:2}]}/>
     </div>
   );
 }
