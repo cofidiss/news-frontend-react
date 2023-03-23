2ba4ec2ac31aa4a891260f4b8c678253cabfe309
sb <sb@450G7>
2023-03-23 19:58:45 +0300

    kısmi navbar yapıldı

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import CommentList from "../src/components/Comment/CommentList/CommentList";
 import NewsCard from "../src/components/NewsCard/NewsCard";
 import AddNews from "../src/components/AddNews/AddNews";
 import NewsList from "../src/components/NewsList/NewsList";
+import NewsNavbar from "./components/NewsNavbar/NewsNavbar";
+
+
 function App() {
   const baseUrl="http://localhost:40774/asdas/asd";
   return (
     <div >
      {/* <NewsCard newsId={11}  baseUrl={baseUrl}/>
      <AddNews categoryId={1}  baseUrl={baseUrl}/> */}
-     <NewsList NewsList={[{header:"heeader1",id:1},{header:"heeader2",id:2}]}/>
+     <NewsNavbar />
     </div>
   );
 }
---
+++ src/components/NewsTab/NewsTab.js
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+
+function NewsTab(props){
+debugger;
+const setSelectedCategoryId = props.setSelectedCategoryId;
+const name = props.name;
+const id = props.id;
+const children = props.children;
+const [anchorEl, setAnchorEl] = React.useState(null);
+const onTabHover = (event) => {
+  setAnchorEl(event.currentTarget);
+};
+const handleClose = (e) => {
+
+  setAnchorEl(null);
+};
+const onCategoryClick = e=> {
+	debugger;
+	console.log("clciked");
+	var selectedCategoryid=	e.currentTarget.getAttribute("categoryid");
+	setSelectedCategoryId(parseInt(selectedCategoryid));
+	setAnchorEl(null);
+}
+const ITEM_HEIGHT = 48;
+return (
+
+<div>
+	
+	<Tab label={name} categoryid={id} onMouseEnter={onTabHover} onClick={onCategoryClick}/> 
+	<Menu  
+	id="long-menu"
+
+	MenuListProps={{
+	  'aria-labelledby': 'long-button',
+	}}
+	anchorEl={anchorEl}
+	open={Boolean(anchorEl)}
+	onClose={handleClose}
+	PaperProps={{
+	  style: {
+		maxHeight: ITEM_HEIGHT * 4.5,
+		width: '20ch',
+	  },
+	}}
+  >
+	{children.map((option) => (
+	  <MenuItem key={option.id} categoryid={option.id}  onClick={onCategoryClick}>
+		{option.name}
+	  </MenuItem>
+	))}
+  </Menu>
+
+</div>  ) }
+
+
+
+	
+	
+
+
+
+
+
+	
+export default NewsTab;
