8cb2f26c38b3b2b073c68ab4bcc819b1ff9b3c56
sinan.balli <sinan.balli@IDB-13217>
2023-03-29 14:38:55 +0300

    eksik olarak news delete yapıldı

--- src/components/NewsList/NewsList.js
+++ src/components/NewsList/NewsList.js
@@ -26,45 +26,81 @@ import Preloader from "../Preloader/Preloader";
 import Modal from "../Modal/Modal";
 import { useNavigate } from "react-router-dom";
 function NewsList(props){
+  debugger;
   const navigate = useNavigate();
-  console.log("NewsList rendered");
+
   const categoryId = props.categoryId;
 const baseUrl = props.baseUrl;
 const [modalState, setModalState] = React.useState({isOpen:false,header:null,content:null,type:null,okOnClick:null,negativeOnClick:null,positiveOnClick:null});
 const [isPreloaderOpenState, setIsPreloaderOpen] = React.useState(false);
 const [newsListState, setnewsListState] = React.useState([]);
-React.useEffect(()=> {fetch(`${baseUrl}/api/News/GetNewsListForCategory?categoryId=${categoryId}`, {
+const [isCategoryAdminState, setIsCategoryAdmin] = React.useState(null);
+
+console.log("NewsList rendered " + newsListState);
+React.useEffect(()=> {setIsPreloaderOpen(true);var getNewPromise = fetch(`${baseUrl}/api/News/GetNewsListForCategory?categoryId=${categoryId}`, {
   method: 'GET', // or 'PUT', 
 
-    })
-  .then((response) => {
-if (!response.ok){
+    }).then((response) => {
+      if (!response.ok){
+      debugger;
+      return Promise.reject("Unknown Error Occured");
+      }
+      return response.json(); }).then(x=> {
+      if (x.hasError){
+      return Promise.reject(x.message);
+      }return x;})
+    var isCategoryAdminPromise  = fetch(`${baseUrl}/api/User/IsCategoryAdmin?categoryId=${categoryId}`, {
+      method: 'POST', // or 'PUT', 
+    
+        }).then((response) => {
+          if (!response.ok){
+          debugger;
+          return Promise.reject("Unknown Error Occured");
+          }
+          return response.json(); }).then(x=> {
+          if (x.hasError){
+          return Promise.reject(x.message);
+          }return x;});
+          Promise.all([getNewPromise,isCategoryAdminPromise])
+  .then(x=> {
 debugger;
-return Promise.reject("Unknown Error Occured");
-}
-return response.json(); }).then(x=> {
-if (x.hasError){
-return Promise.reject(x.message);
-}
-setnewsListState(x);
-
-}, x=> Promise.reject("Unknown Error Occured")
+console.log(11);
+setnewsListState(x[0]);
+setIsCategoryAdmin(x[1]);
+}, x=> {console.log(x);return Promise.reject("Unknown Error Occured")}
 
 ).catch(x=> {
-
+  console.log(112);
 setModalState({isOpen:true,content:x,type:"fail",okOnClick:()=> setModalState({isOpen:false})});
 
 }).finally( () =>setIsPreloaderOpen(false))},[categoryId])
 
+const deleteNews = e=> {
 
+  setIsPreloaderOpen(true);
+fetch(`${baseUrl}/api/News/GetNewsListForCategory?categoryId=${categoryId}`, {
+    method: 'GET', // or 'PUT', 
+  
+      }).then((response) => {
+        if (!response.ok){
+        debugger;
+        return Promise.reject("Unknown Error Occured");
+        }
+        return response.json(); }).then(x=> {
+        if (x.hasError){
+        return Promise.reject(x.message);
+        }return x;})
 
-return (
-  <TableContainer component={Paper}>
+}
+
+return (<div> <Modal isOpen={modalState.isOpen} content={modalState.content} header={modalState.header}  type={modalState.type} okOnClick={modalState.okOnClick} negativeOnClick={modalState.negativeOnClick} positiveOnClick={modalState.positiveOnClick} />
+<Preloader isOpen={isPreloaderOpenState}/>  <TableContainer component={Paper}>
   <Table sx={{ minWidth: 650 }} aria-label="simple table">
     <TableHead>
       <TableRow>
         <TableCell>Header</TableCell>
         <TableCell >UploadDate</TableCell>
+        {isCategoryAdminState === true ?        <TableCell onClick={deleteNews }>Delete</TableCell> : null}
       </TableRow>
     </TableHead>
     <TableBody>
@@ -78,12 +114,13 @@ return (
             {row.header}
           </TableCell>
           <TableCell >{row.uploadDate}</TableCell>
-
+          {isCategoryAdminState === true ?        <TableCell >Delete Button</TableCell> : null}
         </TableRow>
       ))}
     </TableBody>
   </Table>
-</TableContainer>
+</TableContainer></div>
+
 )
 
 
